17e18b6b4c62c61da7658ff1b91e03e63666fef6
Przemek Ścibiorek <pscibiorek@mon.gov.pl>
2023-11-30 09:24:55 +0100

    Work

--- security_controller/settings.py
+++ security_controller/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zjh4)iyh*2h-xvka=+m$8kehg)fxp6q%r8f3wcjuw_j+-6qvfx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -106,6 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
